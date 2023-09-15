05ef9b0221af6dec889e2f97b13eef56ab95051e
kpbaks <kristoffer.pbs@tuta.io>
2023-09-15 21:23:22 +0200

    feat: handle `https://git(hub|lab)/<owner>/<name/`

--- conf.d/_smart-ctrl-v.fish
+++ conf.d/_smart-ctrl-v.fish
@@ -104,9 +104,10 @@ function __smart-ctrl-v.fish::mutate::gh-repo-clone
     for line in $lines
 
         # TODO: <kpbaks 2023-09-14 21:40:37> handle case where (path basename $PWD) == $repo
-        if string match --regex --groups-only "^\s*gh repo clone ([^/]+)/(.+)" $cb \
+        if string match --regex --groups-only "^\s*gh repo clone ([^/]+)/(.+)" $line \
                 | read --line owner repo
-            printf "%s\n" "gh repo clone $owner/$repo && cd $repo && gh repo view --web"
+            # printf "%s\n" "gh repo clone $owner/$repo && cd $repo && gh repo view --web"
+            printf "%s\n" "gh repo clone $owner/$repo && cd $repo"
         else
             printf "%s\n" $line
         end
@@ -123,25 +124,35 @@ function __smart-ctrl-v.fish::mutate::git-clone
             set --append lines $line
         end
     end
-    set --local buffer (commandline)
-    printf "%s\n" $lines
-    # for line in $lines
-    #
-    #     # You ctrl+l && ctrl+c a git url
-    #     if string match --quiet --regex "^(https?|git)://.*\.git\$" -- $line
-    #         # Parse the directory name from the url
-    #         set --append postfix_args (string replace --all --regex '^.*/(.*)\.git$' '$1' $clipboard)
-    #     else if string match --quiet --regex "^git clone .*\.git\$" -- "$clipboard"
-    #         # example: git clone https://github.com/nushell/nushell.git
-    #         set -l url (string replace --all --regex '^git clone (.*)\.git$' '$1' $clipboard)
-    #         set -l reponame (string split --max=1 --right / $url)[-1]
-    #         set -a postfix_args $url
-    #         set -a postfix_args "&& cd $reponame"
-    #     end
-    # end
+    # set --local buffer (commandline)
+
+    if test (commandline | string trim) != ""
+        # The commandline is not empty, so we do not want to mutate the clipboard
+        printf "%s\n" $lines
+        return
+    end
+
+    for line in $lines
+        # You press the "Clone, open or download" button on github
+        if string match --quiet --regex "^'*(https?|git)://.*\.git'*\$" -- $line
+            # Parse the directory name from the url
+            # example: https://github.com/nushell/nushell.git
+            set --local reponame (string split --max=1 --right / $line \
+			| string replace --all "'" "" \
+			| string sub --end=-4)[-1]
+            # printf "%s\n" "git clone --recursive $line && cd $reponame && git show"
+            printf "%s\n" "git clone --recursive $line && cd $reponame && git branch"
+            # You ctrl+l && ctrl+c a git url
+        else if string match --regex --groups-only "^'*https?://git(hub|lab).com/([^/]+)/([^/']+)'*" -- $line | read --line owner reponame
+            # https://github.com/gbprod/yanky.nvim
+            printf "%s\n" "git clone --recursive $line && cd $reponame && git branch"
+        else
+            printf "%s\n" $line
+        end
+    end
 end
 
-function __smart_paste
+function __smart-ctrl-v.fish::paste
     set --local buffer (commandline)
     commandline --insert (fish_clipboard_paste \
         | __smart-ctrl-v.fish::filter::common-leading-whitespace \
@@ -150,52 +161,6 @@ function __smart_paste
         | __smart-ctrl-v.fish::mutate::gh-repo-clone \
         | __smart-ctrl-v.fish::mutate::git-clone)
     commandline --function repaint
-    return
-
-    #
-    # set --local clipboard_content_modified
-    # # Remove leading whitespace, by finding the minimum common leading whitespace of all lines
-    # # and then removing that from all lines.
-    #
-    # set --local length_of_leading_whitespace_of_each_line
-    # for i in (seq (count $clipboard_content))
-    #     set --local line $clipboard_content[$i]
-    #     # TODO: <kpbaks 2023-09-15 08:41:52> handle empty lines
-    #     set --local leading_whitespace (string match --regex --groups-only "^(\s*)" $line)
-    #     set --append length_of_leading_whitespace_of_each_line (string length $leading_whitespace)
-    # end
-    #
-    # set --local minimum_common_leading_whitespace (math min "$(string join "," $length_of_leading_whitespace_of_each_line)")
-    # if test $minimum_common_leading_whitespace -gt 0
-    #     for i in (seq (count $clipboard_content))
-    #         set --local line $clipboard_content[$i]
-    #         set --local line_without_common_leading_whitespace (string sub --start $minimum_common_leading_whitespace $line)
-    #         set --append clipboard_content_modified $line_without_common_leading_whitespace
-    #     end
-    # else
-    #     set clipboard_content_modified $clipboard_content
-    # end
-    #
-    # for i in (seq (count $clipboard_content_modified))
-    #     set --local line $clipboard_content_modified[$i]
-    #     # Remove the leading $ from the clipboard content, so
-    #     # that the command can be executed.
-    #     # This is useful when pasting a command from guides that use
-    #     # a $ to indicate that the command should be run in the terminal.
-    #     set --local line_without_dollar_prefix (string replace --regex "^\s*\\\$\s+" "" -- $line)
-    #     set clipboard_content_modified[$i] $line_without_dollar_prefix
-    # end
-    # # set --local content_to_paste $cb
-    # # set --local content_to_paste (string join " && " $cb)
-    # set --local content_to_paste $clipboard_content_modified
-    # # echo $content_to_paste
-    # # TODO: <kpbaks 2023-09-14 18:59:39> check if the content of the `content_to_paste` is
-    # # syntactically correct. If it is not, then ...
-    # # TODO: <kpbaks 2023-09-14 21:32:46> handle multiline commands formatting better
-    # commandline --insert $content_to_paste
-    # commandline --function repaint
 end
 
-# $ cmake --version
-
-bind \cv __smart_paste
+bind \cv __smart-ctrl-v.fish::paste
